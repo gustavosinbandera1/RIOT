9fd15d94de91ce6b88768fd9e2e3528449ff6854
gustavosinbandera1 <gustavosinbandera1@hotmail.com>
2020-01-22 16:17:55 -0500

    save state

---
+++ examples/aodvv2/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h> // for getting the pid
+#include <stdlib.h>
+
+#include "thread.h"
+#include "shell.h"
+#include "shell_commands.h"
+
+#include "aodvv2/aodvv2.h"
+#include "writer.h"
+
+
+int main(void)
+{
+    aodv_init();
+
+    (void) puts("Welcome to RIOT!");
+    
+    char line_buf[SHELL_DEFAULT_BUFSIZE];
+    shell_run(NULL, line_buf, SHELL_DEFAULT_BUFSIZE);
+
+    return 0;
+}
