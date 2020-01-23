979c0ca598a061362fe8df15469d2e28f33ba1b2
gustavosinbandera1 <gustavosinbandera1@hotmail.com>
2020-01-23 15:37:27 -0500

    first thread working with RAM ok

--- sys/net/routing/aodvv2/aodv.c
+++ sys/net/routing/aodvv2/aodv.c
@@ -36,12 +36,12 @@
 
 static void _init_addresses(void);
 static void _init_sock_snd(void);
-// static void *_aodv_receiver_thread(void *arg);
+static void *_aodv_receiver_thread(void *arg);
 // static void *_aodv_sender_thread(void *arg);
 // static void _deep_free_msg_container(struct msg_container *msg_container);
-// static void _write_packet(struct rfc5444_writer *wr __attribute__ ((unused)),
-//                            struct rfc5444_writer_target *iface __attribute__((unused)),
-//                            void *buffer, size_t length);
+static void _write_packet(struct rfc5444_writer *wr __attribute__ ((unused)),
+                        struct rfc5444_writer_target *iface __attribute__((unused)),
+                        void *buffer, size_t length);
 
 #ifdef DEBUG_ENABLED
 char addr_str[IPV6_MAX_ADDR_STR_LEN];
@@ -51,13 +51,13 @@ static struct netaddr_str nbuf;
 //static char aodv_rcv_stack_buf[KERNEL_CONF_STACKSIZE_MAIN];
 //static char aodv_snd_stack_buf[KERNEL_CONF_STACKSIZE_MAIN];
 
-//static char aodv_rcv_stack_buf[THREAD_STACKSIZE_DEFAULT];
+static char aodv_rcv_stack_buf[THREAD_STACKSIZE_DEFAULT];
 //static char aodv_snd_stack_buf[THREAD_STACKSIZE_DEFAULT];
 
 static aodvv2_metric_t _metric_type;
 // //static int sender_thread;
 static int _sock_snd;
-// static struct autobuf _hexbuf;
+static struct autobuf _hexbuf;
 // static struct sockaddr_in6 sa_wp;
 
 // static ipv6_addr_t /*_v6_addr_local,*/ _v6_addr_mcast/*, _v6_addr_loopback*/;
@@ -75,22 +75,23 @@ void aodv_init(void)
     printf("hola gustavo despierta\n");
     DEBUG("listening on port \n");
     DEBUG("%s()\n", __func__);
+
+
      seqnum_init();
      routingtable_init();
      clienttable_init();
-
      aodv_set_metric_type(AODVV2_DEFAULT_METRIC_TYPE);
-
      _init_addresses();
      _init_sock_snd();
-
 
     // // every node is its own client. 
     clienttable_add_client(&na_local);
     //rreqtable_init();
 
-     aodv_packet_reader_init();
-    // aodv_packet_writer_init(_write_packet);
+
+    // init reader and writer 
+    aodv_packet_reader_init();
+    aodv_packet_writer_init(_write_packet);
 
    
     
@@ -99,26 +100,13 @@ void aodv_init(void)
     /*int if_id = 0;
     net_if_set_src_address_mode(if_id, NET_IF_TRANS_ADDR_M_SHORT);*/
 
-    // aodv_set_metric_type(AODVV2_DEFAULT_METRIC_TYPE);
-    // _init_addresses();
-    // _init_sock_snd();
 
-    // init ALL the things! \o, 
-    // seqnum_init();
-    // routingtable_init();
-    // clienttable_init();
 
-    // every node is its own client. 
-    //clienttable_add_client(&na_local);
-    //rreqtable_init();
 
-    // init reader and writer 
-    //aodv_packet_reader_init();
-    //aodv_packet_writer_init(_write_packet);
 
     // start listening & enable sending 
-    /* thread_create(aodv_rcv_stack_buf, sizeof(aodv_rcv_stack_buf), PRIORITY_MAIN,
-                  CREATE_STACKTEST, _aodv_receiver_thread, NULL, "_aodv_receiver_thread"); */
+    thread_create(aodv_rcv_stack_buf, sizeof(aodv_rcv_stack_buf),
+                  THREAD_PRIORITY_MAIN -1 ,THREAD_CREATE_STACKTEST, _aodv_receiver_thread, NULL, "_aodv_receiver_thread"); 
     
     /*AODV_DEBUG("listening on port %d\n", HTONS(MANET_PORT));
     sender_thread = thread_create(aodv_snd_stack_buf, sizeof(aodv_snd_stack_buf),
@@ -291,56 +279,56 @@ static void _init_sock_snd(void)
 } */
 
 // receive RREQs, RREPs and RERRs and handle them 
-/* static void *_aodv_receiver_thread(void *arg)
+static void *_aodv_receiver_thread(void *arg)
 {
     (void) arg;
 
-    AODV_DEBUG("%s()\n", __func__);
-    uint32_t fromlen;
-    char buf_rcv[UDP_BUFFER_SIZE];
-    msg_t msg_q[RCV_MSG_Q_SIZE];
+    // AODV_DEBUG("%s()\n", __func__);
+    // uint32_t fromlen;
+    // char buf_rcv[UDP_BUFFER_SIZE];
+    // msg_t msg_q[RCV_MSG_Q_SIZE];
 
-    msg_init_queue(msg_q, RCV_MSG_Q_SIZE);
+    // msg_init_queue(msg_q, RCV_MSG_Q_SIZE);
 
-    sockaddr6_t sa_rcv = { .sin6_family = AF_INET6,
-                           .sin6_port = HTONS(MANET_PORT)
-                         };
+    // sockaddr6_t sa_rcv = { .sin6_family = AF_INET6,
+    //                        .sin6_port = HTONS(MANET_PORT)
+    //                      };
 
-    int sock_rcv = socket_base_socket(PF_INET6, SOCK_DGRAM, IPPROTO_UDP);
+    // int sock_rcv = socket_base_socket(PF_INET6, SOCK_DGRAM, IPPROTO_UDP);
 
-    if (-1 == socket_base_bind(sock_rcv, &sa_rcv, sizeof(sa_rcv))) {
-        DEBUG("Error: bind to receive socket failed!\n");
-        socket_base_close(sock_rcv);
-    }
+    // if (-1 == socket_base_bind(sock_rcv, &sa_rcv, sizeof(sa_rcv))) {
+    //     DEBUG("Error: bind to receive socket failed!\n");
+    //     socket_base_close(sock_rcv);
+    // }
 
-    AODV_DEBUG("ready to receive data\n");
-    while (true) {
-        int32_t rcv_size = socket_base_recvfrom(sock_rcv, (void *)buf_rcv, UDP_BUFFER_SIZE, 0,
-                                        &sa_rcv, &fromlen);
+    // AODV_DEBUG("ready to receive data\n");
+    // while (true) {
+    //     int32_t rcv_size = socket_base_recvfrom(sock_rcv, (void *)buf_rcv, UDP_BUFFER_SIZE, 0,
+    //                                     &sa_rcv, &fromlen);
 
-        if (rcv_size < 0) {
-            AODV_DEBUG("ERROR receiving data!\n");
-        }
+    //     if (rcv_size < 0) {
+    //         AODV_DEBUG("ERROR receiving data!\n");
+    //     }
 
-        AODV_DEBUG("_aodv_receiver_thread() %s:",
-              ipv6_addr_to_str(addr_str, IPV6_MAX_ADDR_STR_LEN, &_v6_addr_local));
-        DEBUG(" UDP packet received from %s\n",
-              ipv6_addr_to_str(addr_str, IPV6_MAX_ADDR_STR_LEN, &sa_rcv.sin6_addr));
+    //     AODV_DEBUG("_aodv_receiver_thread() %s:",
+    //           ipv6_addr_to_str(addr_str, IPV6_MAX_ADDR_STR_LEN, &_v6_addr_local));
+    //     DEBUG(" UDP packet received from %s\n",
+    //           ipv6_addr_to_str(addr_str, IPV6_MAX_ADDR_STR_LEN, &sa_rcv.sin6_addr));
 
-        struct netaddr _sender;
-        ipv6_addr_t_to_netaddr(&sa_rcv.sin6_addr, &_sender);
+    //     struct netaddr _sender;
+    //     ipv6_addr_t_to_netaddr(&sa_rcv.sin6_addr, &_sender);
 
-        // For some reason we sometimes get passed our own packets. drop them. 
-        if (netaddr_cmp(&_sender, &na_local) == 0) {
-            AODV_DEBUG("received our own packet, dropping it.\n");
-            aodv_packet_reader_handle_packet((void *) buf_rcv, rcv_size, &_sender);
-        }
-    }
+    //     // For some reason we sometimes get passed our own packets. drop them. 
+    //     if (netaddr_cmp(&_sender, &na_local) == 0) {
+    //         AODV_DEBUG("received our own packet, dropping it.\n");
+    //         aodv_packet_reader_handle_packet((void *) buf_rcv, rcv_size, &_sender);
+    //     }
+    // }
 
-    socket_base_close(sock_rcv);
+    // socket_base_close(sock_rcv);
 
     return NULL;
-} */
+} 
 
 /* ipv6_addr_t *aodv_get_next_hop(ipv6_addr_t *dest)
 {
@@ -457,40 +445,40 @@ static void _init_sock_snd(void)
  * Handle the output of the RFC5444 packet creation process. This callback is
  * called by every writer_send_* function.
  */
-// static void _write_packet(struct rfc5444_writer *wr __attribute__ ((unused)),
-//                           struct rfc5444_writer_target *iface __attribute__((unused)),
-//                           void *buffer, size_t length)
-// {
-//     AODV_DEBUG("%s()\n", __func__);
-//     (void)buffer;
-//     (void)length;
-//     // // generate hexdump and human readable representation of packet
-//     //  //and print to console 
-//     // abuf_hexdump(&_hexbuf, "\t", buffer, length);
-//     // rfc5444_print_direct(&_hexbuf, buffer, length);
-//     // DEBUG("%s", abuf_getptr(&_hexbuf));
-//     // abuf_clear(&_hexbuf);
+static void _write_packet(struct rfc5444_writer *wr __attribute__ ((unused)),
+                          struct rfc5444_writer_target *iface __attribute__((unused)),
+                          void *buffer, size_t length)
+{
+    DEBUG("%s()\n", __func__);
+    (void)buffer;
+    (void)length;
+    // // generate hexdump and human readable representation of packet
+    //  //and print to console 
+    abuf_hexdump(&_hexbuf, "\t", buffer, length);
+    // rfc5444_print_direct(&_hexbuf, buffer, length);
+    // DEBUG("%s", abuf_getptr(&_hexbuf));
+    // abuf_clear(&_hexbuf);
 
-//     // //  fetch the address the packet is supposed to be sent to (i.e. to a
-//     // //   specific node or the multicast address) from the writer_target struct
-//     // //  iface* is stored in. This is a bit hacky, but it does the trick. 
-//     // wt = container_of(iface, struct writer_target, interface);
-//     // netaddr_to_ipv6_addr_t(&wt->target_addr, (ipv6_addr_t*)&sa_wp.sin6_addr);
+    // //  fetch the address the packet is supposed to be sent to (i.e. to a
+    // //   specific node or the multicast address) from the writer_target struct
+    // //  iface* is stored in. This is a bit hacky, but it does the trick. 
+    // wt = container_of(iface, struct writer_target, interface);
+    // netaddr_to_ipv6_addr_t(&wt->target_addr, (ipv6_addr_t*)&sa_wp.sin6_addr);
 
-//     // // When originating a RREQ, add it to our RREQ table/update its predecessor 
-//     // if (wt->type == RFC5444_MSGTYPE_RREQ
-//     //     && netaddr_cmp(&wt->packet_data.origNode.addr, &na_local) == 0) {
-//     //     AODV_DEBUG("originating RREQ with SeqNum %d towards %s via %s; updating RREQ table...\n",
-//     //           wt->packet_data.origNode.seqnum,
-//     //           netaddr_to_string(&nbuf, &wt->packet_data.targNode.addr),
-//     //           ipv6_addr_to_str(addr_str, IPV6_MAX_ADDR_STR_LEN, &sa_wp.sin6_addr));
-//     //     rreqtable_is_redundant(&wt->packet_data);
-//     // }
+    // // When originating a RREQ, add it to our RREQ table/update its predecessor 
+    // if (wt->type == RFC5444_MSGTYPE_RREQ
+    //     && netaddr_cmp(&wt->packet_data.origNode.addr, &na_local) == 0) {
+    //     AODV_DEBUG("originating RREQ with SeqNum %d towards %s via %s; updating RREQ table...\n",
+    //           wt->packet_data.origNode.seqnum,
+    //           netaddr_to_string(&nbuf, &wt->packet_data.targNode.addr),
+    //           ipv6_addr_to_str(addr_str, IPV6_MAX_ADDR_STR_LEN, &sa_wp.sin6_addr));
+    //     rreqtable_is_redundant(&wt->packet_data);
+    // }
 
-//     // size_t bytes_sent =  sendto(_sock_snd, buffer, length, 0, (struct sockaddr*)&sa_wp, sizeof(sa_wp));
-//     // (void) bytes_sent;
-//     // AODV_DEBUG("%d bytes sent.\n", bytes_sent);
-// }
+    // size_t bytes_sent =  sendto(_sock_snd, buffer, length, 0, (struct sockaddr*)&sa_wp, sizeof(sa_wp));
+    // (void) bytes_sent;
+    // AODV_DEBUG("%d bytes sent.\n", bytes_sent);
+}
 
 
 // free the matryoshka doll of cobbled-together structs that the sender_thread receives 
