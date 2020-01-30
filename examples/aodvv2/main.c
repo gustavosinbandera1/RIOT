1289804b47f91ad2f79977bc4c44f5ffc92a4efb
gustavosinbandera1 <gustavosinbandera1@hotmail.com>
2020-01-30 01:20:42 -0500

    add more neighbors funcion

--- examples/aodvv2/main.c
+++ examples/aodvv2/main.c
@@ -18,13 +18,56 @@
 #include <netinet/in.h>
 #include <sys/socket.h>
 
+#include "eui64.h"
+#include "net/gnrc/ipv6/nib.h"
+
+
+#include "net/gnrc/ipv6/nib.h"
+#include "net/gnrc/ipv6/nib/nc.h"
+
+//#include "netreg.h"
+
+#include "net/gnrc/pktdump.h"
+#include "net/gnrc.h"
+#include "od.h"
+#include "net/netdev_test.h"
+#include "xtimer.h"
+
 
 //#include "udp.h"
 //#include "byteorder.h"
 
 //#include "net/gnrc/netif.h"
+//add_neighbor fe80::55:44:33:ff:fe:22:11:00 57:44:33:22:11:00
+#define DUMPER_QUEUE_SIZE   (16)
 
+#define NBR_MAC             { 0x57, 0x44, 0x33, 0x22, 0x11, 0x00, }
+#define NBR_LINK_LOCAL      { 0xfe, 0x80, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, \
+                              0x55, 0x44, 0x33, 0xff, 0xfe, 0x22, 0x11, 0x00, }
+#define DST                 { 0x20, 0x01, 0x0d, 0xb8, 0x00, 0x00, 0xab, 0xcd, \
+                              0x55, 0x44, 0x33, 0xff, 0xfe, 0x22, 0x11, 0x00, }
+#define DST_PFX_LEN         (64U)
 
+/* IPv6 header + payload:https://github.com/gustavosinbandera1/RIOT     version+TC  FL: 0       plen: 16    NH:17 HL:64 */
+#define L2_PAYLOAD          { 0x60, 0x00, 0x00, 0x00, 0x00, 0x10, 0x11, 0x40, \
+                              /* source: random address */                    \
+                              0x20, 0x01, 0x0d, 0xb8, 0x00, 0x00, 0xef, 0x01, \
+                              0x02, 0xca, 0x4b, 0xef, 0xf4, 0xc2, 0xde, 0x01, \
+                              /* destination: DST */                          \
+                              0x20, 0x01, 0x0d, 0xb8, 0x00, 0x00, 0xab, 0xcd, \
+                              0x55, 0x44, 0x33, 0xff, 0xfe, 0x22, 0x11, 0x00, \
+                              /* random payload of length 16 */               \
+                              0x54, 0xb8, 0x59, 0xaf, 0x3a, 0xb4, 0x5c, 0x85, \
+                              0x1e, 0xce, 0xe2, 0xeb, 0x05, 0x4e, 0xa3, 0x85, }
+
+//static char addr_str[IPV6_ADDR_MAX_STR_LEN];
+static const uint8_t _nbr_mac[] = NBR_MAC;
+static const ipv6_addr_t _nbr_link_local = { .u8 = NBR_LINK_LOCAL };
+// static const ipv6_addr_t _dst = { .u8 = DST };
+// static const uint8_t _l2_payload[] = L2_PAYLOAD;
+// static gnrc_netreg_entry_t _dumper;
+// static msg_t _dumper_queue[DUMPER_QUEUE_SIZE];
+// static char _dumper_stack[THREAD_STACKSIZE_MAIN];
 
 
 
@@ -43,27 +86,57 @@ static int _sock_snd;
 struct sockaddr_in6 _sockaddr;
 
 msg_t msg_q[RCV_MSG_Q_SIZE];
-//gnrc_netif_t *ieee802154_netif;
+gnrc_netif_t *_mock_netif = NULL;
+timex_t _now;
 
 char _rcv_stack_buf[THREAD_STACKSIZE_MAIN];
 
-static void _demo_init_socket(void);
-int demo_print_routingtable(int argc, char** argv);
+static void init_socket(void);
+int show_routingtable(int argc, char** argv);
+int demo_send(int argc, char** argv);
+int demo_attempt_to_send(char* dest_str, char* msg);
 static void *_demo_receiver_thread(void *arg);
 
 
 
+extern int udp_cmd(int argc, char **argv);
+int demo_add_neighbor(int argc, char** argv);
+//static void *_demo_receiver_thread(void *arg);
+
+
+
 const shell_command_t shell_commands[] = {
-    {"print_rt", "print routingtable", demo_print_routingtable},
+    {"print_rt", "print routingtable", show_routingtable},
+    { "udp", "send data over UDP and listen on UDP ports", udp_cmd },
+    {"add_neighbor", "add neighbor to Neighbor Cache", demo_add_neighbor},
+    {"send", "send message to ip", demo_send},
     {NULL, NULL, NULL}
 };
 
 int main(void)
 { 
+   void *iter_state = NULL;
+   gnrc_ipv6_nib_nc_t nce;
+
     msg_init_queue(msg_q, RCV_MSG_Q_SIZE);
-    _demo_init_socket();
+    init_socket();
     aodv_init();
     (void) puts("Welcome to RIOT!");
+    _mock_netif = gnrc_netif_iter(_mock_netif);
+    if (_mock_netif != NULL) {
+        DEBUG("tenemos una interface PID: %d\n",_mock_netif->pid);
+    }
+
+
+    gnrc_ipv6_nib_init();
+    gnrc_netif_acquire(_mock_netif);
+    gnrc_ipv6_nib_init_iface(_mock_netif);
+    gnrc_netif_release(_mock_netif);
+
+    while (gnrc_ipv6_nib_nc_iter(0, &iter_state, &nce)) {
+      gnrc_ipv6_nib_nc_print(&nce);
+    }
+    printf("LA IFACE ES: %u",gnrc_ipv6_nib_nc_get_iface (&nce));
 
 
     thread_create(_rcv_stack_buf, sizeof(_rcv_stack_buf), 
@@ -78,10 +151,10 @@ int main(void)
     return 0;
 }
 
-static void _demo_init_socket(void)
+static void init_socket(void)
 {
     _sockaddr.sin6_family = AF_INET6;
-    _sockaddr.sin6_port = (int) byteorder_htons(RANDOM_PORT).u8;
+    _sockaddr.sin6_port = (int)htons(RANDOM_PORT);
 
     _sock_snd = socket(AF_INET6, SOCK_DGRAM, IPPROTO_UDP);
 
@@ -92,7 +165,7 @@ static void _demo_init_socket(void)
 }
 
 
-int demo_print_routingtable(int argc, char** argv)
+int show_routingtable(int argc, char** argv)
 {
     (void)argc;
     (void)argv;
@@ -100,6 +173,97 @@ int demo_print_routingtable(int argc, char** argv)
     return 0;
 }
 
+/*
+    Help emulate a functional NDP implementation (this should be called for every
+    neighbor of the node on the grid)
+*/
+int demo_add_neighbor(int argc, char** argv)
+{
+    if (argc != 3) {
+        printf("Usage: add_neighbor <neighbor ip> <neighbor ll-addr>\n");
+       //return 1;
+    }
+  (void)_nbr_link_local;
+  (void)_mock_netif;
+  
+    void *iter_state = NULL;
+   gnrc_ipv6_nib_nc_t nce;
+  int res;
+(void)argv;
+    eui64_t eut_eui64;
+    (void)eut_eui64;
+    ipv6_addr_t neighbor;
+    (void)neighbor;
+    (void)_nbr_mac;
+   // inet_pton(AF_INET6, argv[1], &neighbor); //converts an IPv4 or IPv6 Internet network address in its standard text presentation form into its numeric binary form.
+// inet_pton(AF_INET6, (char*)&_nbr_link_local, &neighbor); 
+inet_pton(AF_INET6, argv[1], &neighbor); 
+
+    /* define neighbor to forward to */
+    // res = gnrc_ipv6_nib_nc_set(&_nbr_link_local, _mock_netif->pid,
+    //                             _nbr_mac, sizeof(_nbr_mac));
+     res = gnrc_ipv6_nib_nc_set(&neighbor, _mock_netif->pid,
+                                 (uint8_t*)&neighbor.u8[16], sizeof(neighbor.u8[14]));
+    (void)res;
+  //  printf("agregando neigh:  %d", res);
+
+    while (gnrc_ipv6_nib_nc_iter(0, &iter_state, &nce)) {
+      gnrc_ipv6_nib_nc_print(&nce);
+    }
+    printf("la iface es %u",gnrc_ipv6_nib_nc_get_iface(&nce)); 	
+
+    printf("el estado es %d",gnrc_ipv6_nib_nc_get_nud_state(&nce	));
+
+    return 1;
+}
+
+int demo_send(int argc, char** argv)
+{
+    if (argc != 3) {
+        printf("Usage: send <destination ip> <message>\n");
+        return 1;
+    }
+
+    char* dest_str = argv[1] ;
+    char* msg = argv[2];
+
+    return demo_attempt_to_send(dest_str, msg);
+}
+
+
+int demo_attempt_to_send(char* dest_str, char* msg)
+{
+    uint8_t num_attempts = 0;
+
+    // turn dest_str into ipv6_addr_t
+    inet_pton(AF_INET6, dest_str, &_sockaddr.sin6_addr);
+    int msg_len = strlen(msg)+1;
+    (void)msg_len;
+    xtimer_now_timex(&_now);
+     
+
+    printf("{%" PRIu32 ":%" PRIu32 "}[demo]   sending packet of %i bytes towards %s...\n", _now.seconds, _now.microseconds, msg_len, dest_str);
+
+    while(num_attempts < DISCOVERY_ATTEMPTS_MAX) {
+        int bytes_sent = sendto(_sock_snd, msg, msg_len,
+                                                0, (struct sockaddr*)&_sockaddr, sizeof _sockaddr);
+
+        printf("los bytes enviados son %d", bytes_sent);
+
+         xtimer_now_timex(&_now);
+        if (bytes_sent == -1) {
+            printf("{%" PRIu32 ":%" PRIu32 "}[demo]   no bytes sent, probably because there is no route yet.\n", _now.seconds, _now.microseconds);
+            num_attempts++;
+            xtimer_usleep(RREQ_WAIT_TIME);
+         }
+         else {
+            printf("{%" PRIu32 ":%" PRIu32 "}[demo]   Success sending Data: %d bytes sent.\n", _now.seconds, _now.microseconds, bytes_sent);
+            return 0;
+         }
+    } 
+    printf("{%" PRIu32 ":%" PRIu32 "}[demo]  Error sending Data: no route found\n", _now.seconds, _now.microseconds);
+    return -1;
+}
 
 static void *_demo_receiver_thread(void *arg)
 {
@@ -127,7 +291,7 @@ static void *_demo_receiver_thread(void *arg)
        puts("errors initializing socket\n");
     }
 
-    if (-1 == bind(sock_rcv,(struct sockaddr*)&sa_rcv, sizeof(sa_rcv))) {
+    if (-1 == bind(sock_rcv,(struct sockaddr*)&sa_rcv.sin6_addr, sizeof(sa_rcv.sin6_addr))) {
          DEBUG("[demo]   Error: bind to receive socket failed!\n");
          close(sock_rcv);
          
@@ -148,4 +312,4 @@ static void *_demo_receiver_thread(void *arg)
 
     // socket_base_close(sock_rcv);
     return NULL;
-}
+} 
